94f89879122258be4fc2cfbcb7bbcd543a7fb005
Fbrend23 <158816892+Fbrend23@users.noreply.github.com>
2024-03-14 19:45:40 +0100

    Update

--- index.js
+++ index.js
@@ -2,13 +2,16 @@ document.addEventListener("DOMContentLoaded", function(){
     setTimeout(function(){
         const intro1 = document.getElementsByClassName("intro1");
         for (let i = 0; i < intro1.length; i++){
-            intro1[i].style.opacity ="1";
-    }
-}, 1000);
+            intro1[i].style.opacity ="1";}}, 1000);
+    setTimeout(function(){
+        document.querySelector(".intro2").style.opacity = "1";}, 2000);
 })
 
-document.addEventListener("DOMContentLoaded",function(){
-    setTimeout(function(){
-        document.getElementsByClassName("intro2")[0].style.opacity = "1";
-    }, 2000);
+const menu = document.getElementById("hamburger");
+const bar = document.querySelectorAll(".bar1, .bar2, .bar3");
+
+bar.forEach(function(bar){
+menu.addEventListener("click", function() {
+    bar.classList.toggle("click");
 })
+});
